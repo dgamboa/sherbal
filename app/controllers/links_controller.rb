9012248f75210f6adb7f112a5d9cf445655b7f80
Daniel Gamboa <danielgamboa10@gmail.com>
2012-09-25 10:06:28 -0500

    Fix links controller and add formatting

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,16 +9,16 @@ class LinksController < ApplicationController
   end
 
   def create
-  	@link = Link.new(params[:link])
+    @link = Link.new(params[:link])
 
-		respond_to do |format|
-			if @link.save
-				format.html {render :action => "show"}
-				format.json {render :json => @link}
-			else
-				format.html {render :action => "new"}
-				format.json {render :json => @link.errors, :status => :unprocessable_entity}
-			end
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.json { render json: @link, status: :created, location: @link }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
 		end
   end
 end
