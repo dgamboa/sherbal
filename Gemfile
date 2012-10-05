8abf63ebc807460ea95264b557eed9551755565e
DavidRagone <dmragone@gmail.com>
2012-10-04 21:40:25 -0700

    Annotate models, change link order to number of votes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.8'
 gem 'bootstrap-sass', '2.0.4'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'annotate'
 
 gem 'devise'
 gem 'will_paginate', '~> 3.0'
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  link_id    :integer
+#  up         :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Vote < ActiveRecord::Base
 	attr_accessible :link_id, :up
 	validates :user_id, :uniqueness => { :scope => :link_id }
